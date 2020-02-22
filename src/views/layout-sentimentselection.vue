8c93d021f7cd62ed5e7fd9f118c87fbb63298749
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-02-22 23:32:36 +0800

    added sentiment selection page

--- src/views/layout-sentimentselection.vue
+++ src/views/layout-sentimentselection.vue
@@ -9,7 +9,7 @@
 			<div class="content-container">
 				<div class="greeting">
 					<h1>
-						Hi <span id="employee-name">{{ employee }}!</span><br>
+						Hello, <span id="employee-name">{{ employee }}!</span><br />
 						How are you feeling today?
 					</h1>
 				</div>
@@ -33,15 +33,42 @@
 				</div>
 
 				<span class="bottom-tip">
-					(Please let us know what you honestly feel today)
+					Choose an emotion that best describes what you feel right now.
 				</span>
+
+				<div id="countdown">
+					<div id="countdown-number"></div>
+					<svg>
+						<circle r="15" cx="20" cy="20"></circle>
+					</svg>
+				</div>
+
+				<div class="chosen_emotion_container" v-if="showEmotionOverlay">
+					<div class="overlay">
+						<img
+							:src="`/emotions/${emotionPicked || 5}.svg`"
+							id="chosen_emotion_image"
+						/>
+					</div>
+				</div>
 			</div>
 		</form>
 
+		<div v-else-if="errorMessage" id="error">
+			<h1>{{ errorMessage }}</h1>
+		</div>
+
 		<div v-else class="welcome">
-			<img src="@/assets/images/love.png" id="love" />
-			<img src="@/assets/images/iACADEMY_Seal.png" alt="iacademy" />
-			<h1>Good morning <span class="gamechanger">GAME CHANGER!</span></h1>
+			<img
+				src="@/assets/images/iACADEMY_Seal.png"
+				alt="iacademy"
+				id="iacademy_logo"
+			/>
+			<h1>
+				<img :src="`${dayGreeting}.svg`" id="timeGraphics" :alt="dayGreeting" />
+				<span>Good {{ dayGreeting }} </span>
+				<span class="gamechanger">GAME CHANGER!</span>
+			</h1>
 			<h2>Please log your attendance</h2>
 		</div>
 	</div>
@@ -49,36 +76,105 @@
 
 <script>
 import { mapGetters } from "vuex";
+import anime from "animejs";
 
 export default {
 	name: "EmotionSelection",
 	data() {
 		return {
-
+			emotionPicked: 0,
+			showEmotionOverlay: false,
+			timeOut: null
 		};
 	},
 	computed: {
 		...mapGetters({
 			employeeLog: "emotions/employeeLog",
 			employee: "emotions/employee",
-			status: "emotions/status"
-		})
+			status: "emotions/status",
+			errorMessage: "emotions/errorMessage"
+		}),
+		dayGreeting() {
+			let time = new Date().getHours();
+
+			if (time > 0 && time < 12) {
+				return "morning";
+			} else if (time >= 12 && time < 18) {
+				return "afternoon";
+			} else {
+				return "evening";
+			}
+		}
 	},
 	methods: {
 		async submitEmotion(emotion) {
+			this.emotionPicked = emotion;
+			this.show();
+
 			try {
-				this.$store.dispatch("emotions/submitSentiment", {
+				this.$store.dispatch("emotions/SUBMIT_SENTIMENT", {
 					emotion: emotion,
 					employeeLog: this.employeeLog,
 					status: this.status
 				});
 
+				clearTimeout(this.timeOut);
 			} catch (error) {
 				console.log(error);
 			}
+		},
+		async show() {
+			this.showEmotionOverlay = true;
+
+			 await setTimeout(() => {
+				this.showEmotionOverlay = false;
+				this.$store.commit("emotions/CLEAR");
+			}, 1500);
+
+			anime
+				.timeline({
+					targets: ".chosen_emotion_container",
+					zIndex: "2",
+					easing: "easeInOutBack",
+					delay: "-1000"
+				})
+				.add(
+					{
+						targets: ".overlay",
+						opacity: 1
+					},
+					"500"
+				)
+				.add(
+					{
+						targets: "#chosen_emotion_image",
+						scale: 1.3
+					},
+					"900"
+				)
+				.add(
+					{
+						targets: ".overlay",
+						opacity: 0
+					},
+					"1000"
+				);
 		}
 	},
-	sockets: {}
+	sockets: {
+		async employeeLog() {
+			clearTimeout(this.timeOut);
+			this.timeOut = setTimeout(() => {
+				this.$store.commit("emotions/CLEAR");
+			}, 15000);
+		},
+		async logError() {
+			clearTimeout(this.timeOut);
+			this.timeOut = setTimeout(() => {
+				this.$store.commit("emotions/CLEAR");
+			}, 4000);
+		}
+	}
 };
 </script>
 
@@ -104,7 +200,7 @@ export default {
 	display: flex;
 	justify-content: center;
 	align-items: center;
-	font-family: "Google Sans", sans-serif;
+	font-family: "Cera Pro", sans-serif;
 	// background-color: aquamarine;
 }
 
@@ -118,15 +214,15 @@ export default {
 		text-align: center;
 		color: white;
 		font-size: 30px;
-		text-shadow: 2px 3px 0px #0000003a;
+		text-shadow: 2px 2px 0px #5535fc;
 		line-height: 70px;
 		// margin-bottom: 20px;
 	}
 
 	.bottom-tip {
 		margin-top: 10px;
-		color: white;
-		font-size: 18px;
+		color: #ffffff;
+		font-size: 20px;
 	}
 }
 
@@ -147,7 +243,7 @@ export default {
 		}
 
 		.emotion-image {
-			height: 150px;
+			height: 165px;
 		}
 	}
 }
@@ -158,11 +254,15 @@ export default {
 	justify-content: center;
 	align-items: center;
 	position: relative;
+	margin-top: 20px;
 
-	#love {
-		position: absolute;
-		right: 170px;
-		top: -40px;
+	#timeGraphics {
+		margin-right: 10px;
+		height: 50px;
+	}
+
+	#iacademy_logo {
+		height: 190px;
 	}
 
 	h1 {
@@ -180,8 +280,195 @@ export default {
 		font-weight: normal;
 		color: white;
 		text-shadow: 2px 2px 0px #5535fc;
-		font-size: 35px;
+		font-size: 38px;
 		margin-top: -10px;
+	}
+}
+
+#error {
+	font-size: 20px;
+	color: white;
+}
+
+#countdown {
+	position: relative;
+	margin-top: 30px;
+	height: 40px;
+	width: 40px;
+	text-align: center;
+
+	#countdown-number {
+		color: white;
+		display: inline-block;
+		line-height: 40px;
+	}
+
+	svg {
+		position: absolute;
+		top: 0;
+		right: 0;
+		width: 40px;
+		height: 40px;
+		transform: rotateY(-180deg) rotateZ(-90deg);
+	}
+
+	svg circle {
+		stroke-dasharray: 113px;
+		stroke-dashoffset: 0px;
+		stroke-linecap: round;
+		stroke-width: 2px;
+		stroke: white;
+		fill: none;
+		animation: countdown 15s linear infinite forwards;
+	}
+}
+
+.chosen_emotion_container {
+	position: absolute;
+	height: 100vh;
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	z-index: -1;
+	// background-color: #ff0000;
+
+	.overlay {
+		position: absolute;
+		height: 100vh;
+		width: 100%;
+		background-color: #00000079;
+		opacity: 0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	#chosen_emotion_image {
+		position: absolute;
+	}
+}
+
+@keyframes countdown {
+	from {
+		stroke-dashoffset: 0px;
+	}
+	to {
+		stroke-dashoffset: 113px;
+	}
+}
+
+@media (max-width: 900px) {
+	#emotion-selection-form {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		font-family: "Cera Pro", sans-serif;
+		// background-color: aquamarine;
+	}
+
+	.content-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+
+		.greeting {
+			text-align: center;
+			color: white;
+			font-size: 20px;
+			text-shadow: 2px 2px 0px #5535fc;
+			line-height: 48px;
+			// margin-bottom: 20px;
+		}
+
+		// #employee-name {
+		// 	color: #92FFDA;
+		// }
+
+		.bottom-tip {
+			margin-top: 10px;
+			font-size: 15px;
+		}
+	}
+
+	.emotion-container {
+		// background-color: #f5f5f5;
+		padding: 0px;
+		margin: 0px;
+		display: flex;
+		margin-top: 10px;
+
+		button {
+			background: #ffffff00;
+			padding: 0px;
+			margin: 0px 5px;
+			border: none;
+			display: flex;
+
+			&:hover {
+				cursor: pointer;
+			}
+
+			.emotion-image {
+				height: 110px;
+			}
+		}
+	}
+
+	.welcome {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		position: relative;
+		margin-top: 20px;
+
+		#timeGraphics {
+			height: 40px;
+		}
+
+		#iacademy_logo {
+			height: 150px;
+		}
+
+		h1 {
+			font-size: 40px;
+			letter-spacing: -1px;
+			color: white;
+			text-shadow: 2px 2px 0px #5535fc;
+
+			.gamechanger {
+				font-weight: bolder;
+			}
+		}
+
+		h2 {
+			font-weight: normal;
+			color: white;
+			text-shadow: 2px 2px 0px #5535fc;
+			font-size: 28px;
+			margin-top: -10px;
+		}
+	}
+
+	#error {
+		font-size: 15px;
+		color: white;
+		// background-color: #5535fc;
+		line-height: 43px;
+		width: 70%;
+		text-align: center;
+		font-weight: normal;
+	}
+
+	#countdown {
+		position: relative;
+		margin-top: 10px;
+		height: 40px;
+		width: 40px;
+		text-align: center;
 	}
 }
 </style>
