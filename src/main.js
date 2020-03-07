4fb5d45e19d408b61d465be0599c3682e47f56cd
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-07 13:28:46 +0800

    temporarily disabled vue-sessions

--- src/main.js
+++ src/main.js
@@ -7,19 +7,22 @@ import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import VueSocketIO from 'vue-socket.io';
-import VueSession from 'vue-session';
+// import VueSession from 'vue-session';
 
 Vue.config.productionTip = false
 
 const port = 3000;
-// const server_url = `http://192.168.1.5:${port}/`;
 const server_url = `http://192.168.1.5:${port}/`;
 
-// AXIOS
+// AXIOS -------------------------------------------------------------------------------------------------
 axios.defaults.baseURL = server_url;
 Vue.use(VueAxios, axios);
 
-// SOCKET INTEGRATION
+// Vue.use(VueSession, {
+//   persist: true
+// })
+
+// SOCKET INTEGRATION ------------------------------------------------------------------------------------
 Vue.use(new VueSocketIO({
   debug: true,
   connection: server_url,
@@ -34,6 +37,5 @@ new Vue({
   router,
   store,
   vuetify,
-  VueSession,
   render: h => h(App)
 }).$mount('#app')
