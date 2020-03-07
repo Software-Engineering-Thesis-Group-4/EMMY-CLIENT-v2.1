595d4775f1c48e8dcd803a1b9a9dc1a7bc0896bb
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-07 13:29:55 +0800

    added output directory config for production release

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
+const path = require('path');
+
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  outputDir: path.resolve(__dirname, '../EMMY-SERVER/public'),
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http:///localhost:3000'
+      }
+    }
+  }
 }
