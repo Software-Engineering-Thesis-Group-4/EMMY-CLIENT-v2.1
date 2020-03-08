d86b8e862ce10d86f9e3f596bbb212c57efa94bb
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-08 16:33:44 +0800

    added user credentials in user store

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -3,31 +3,30 @@ import Vue from 'vue'
 const UserModule = {
     namespaced: true,
     state: {
-        error   : null,
-        username: null,
-        email   : null,
+        error    : null,
+        username : null,
+        email    : null,
     },
     mutations: {
         LOGIN_ERROR(state, message) {
             state.error = message;
         },
         AUTH_SUCCESS(state, payload) {
-            state.error = null;
-            state.username = payload.username;
-            state.email = payload.email;
+            state.error     = null;
+            state.username  = payload.username;
+            state.email     = payload.email;
         }
     },
     actions: {
         async LOGIN(context, { email, password }) {
             try {
-                // create login request to server (expected response object is [x] sessionId, [x] email, [x] username)
                 let response = await Vue.axios.post('/auth/login', { email, password });
                 context.commit('AUTH_SUCCESS', response.data);
                 return response;
 
             } catch (error) {
                 console.dir(error.response);
-                context.commit('LOGIN_ERROR', error.response.data);
+                context.commit('LOGIN_ERROR', error.response);
             }
         },
         LOGOUT() {
@@ -36,6 +35,8 @@ const UserModule = {
     },
     getters: {
         errorMessage: state => state.error,
+        username: state => state.username,
+        email: state => state.email,
     }
 }
 
