4545ee51a641361eb558882527fc538db9e5c15b
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-10 16:10:43 +0800

    disabled vue-session and vue-cookies

--- src/main.js
+++ src/main.js
@@ -2,25 +2,32 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
 
+import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import VueSocketIO from 'vue-socket.io';
 // import VueSession from 'vue-session';
+// import VueCookie from 'vue-cookie';
 
 Vue.config.productionTip = false
 
 const port = 3000;
-const server_url = `http://192.168.1.5:${port}/`;
+const host = "192.168.1.5";
+const server_url = `http://${host}:${port}/`;
 
 // AXIOS -------------------------------------------------------------------------------------------------
 axios.defaults.baseURL = server_url;
+
 Vue.use(VueAxios, axios);
 
+// VUE SESSION INTEGRATION -------------------------------------------------------------------------------
 // Vue.use(VueSession, {
 //   persist: true
 // })
+
+// VUE COOKIE PLUGIN -------------------------------------------------------------------------------------
+// Vue.use(VueCookie);
 
 // SOCKET INTEGRATION ------------------------------------------------------------------------------------
 Vue.use(new VueSocketIO({
@@ -33,7 +40,7 @@ Vue.use(new VueSocketIO({
   },
 }));
 
-new Vue({
+new Vue({  
   router,
   store,
   vuetify,
