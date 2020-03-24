04e08326c86fffdc1ff23f349103f5baca08c238
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-25 01:05:06 +0800

    made Overall employee sentiment as a separate component

---
+++ src/components/dashboard/OverallEmployeeSentiment.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="container" :elevation="3">
+		<apexchart
+			ref="chart"
+			id="OSP-stacked-bars"
+			type="bar"
+			height="380"
+			:options="chartData.chartOptions"
+			:series="chartData.series"
+		></apexchart>
+	</div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+import apexchart from "vue-apexcharts";
+import { chartData, updateData } from "@/dashboard/chart.overall_sentiment.js";
+
+export default {
+	data() {
+		return {
+			chartData
+		};
+	},
+	components: {
+		apexchart
+	},
+	computed: {
+		...mapGetters({
+			logs: `employees/attendanceLogs`
+		}),
+	},
+	created() {
+		this.$store.dispatch("employees/FETCH_ATTENDANCELOGS").then(logs => {
+			this.$refs.chart.updateSeries(updateData(logs), true);
+		});
+	},
+
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+	background-color: #ffffff;
+	display: flex;
+	flex-direction: column;
+	border: 1px solid #e0e0e0;
+	// box-shadow: 0px 3px 10px 2px #0000000e;
+	border-radius: 8px;
+	padding: 20px 20px 0px;
+}
+</style>
