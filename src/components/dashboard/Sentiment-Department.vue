097307a332e451771363bfcf479b948cbee8da36
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-26 03:15:39 +0800

    added gender and department components

---
+++ src/components/dashboard/Sentiment-Department.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="chart-container"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.chart-container {
+	background-color: pink;
+	width: 100%;
+}
+</style>
---
+++ src/components/dashboard/Sentiment-Gender.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="chart-container">
+		<!-- <apexcharts ref="genderChart" type="donut" /> -->
+	</div>
+</template>
+
+<script>
+// import apexcharts from "vue-apexcharts";
+
+
+export default {
+	components: {
+		// apexcharts
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.chart-container {
+	background-color: cyan;
+	width: 100%;
+}
+</style>
