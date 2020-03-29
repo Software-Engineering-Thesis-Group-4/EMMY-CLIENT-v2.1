a790f92fa6273795e64a4b063c86f2a72c1ca373
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-03-29 19:44:00 +0800

    fixed add employee form

--- src/components/AddEmployeeForm.vue
+++ src/components/AddEmployeeForm.vue
@@ -1,216 +1,359 @@
 <template>
-	<div>
-		<!-- FIXME: Change the form into vuetify's form -->
-		<!-- TODO: add the upload csv button for adding multiple employees -->
-		<form method="POST" v-on:submit.prevent="addEmployee">
-			<h1>Add New Employee</h1>
+  <v-card class="form_container">
+    <v-toolbar 
+      flat
+      dense
+      dark
+      color="#567DD8"
+      short
+      min-width="100%"
+    >
+      <span>Register Employees</span>
 
-			<label for="firstname">Firstname</label>
-			<input type="text" name="firstname" v-model="formData.firstname" />
+      <v-btn 
+        class="fileUploadToggleButton"
+        small
+        dark
+        outlined
+        @click="multipleUpload = !multipleUpload"
+      >
+        <span v-if="multipleUpload">&lt; Back</span>
+        <span v-else>Import CSV</span>
+      </v-btn>
+    </v-toolbar>
 
-			<label for="">Lastname</label>
-			<input type="text" name="lastname" v-model="formData.lastname" />
+    <!-- <p class="upload_file-label">Upload a CSV file:</p> -->
+    <v-form 
+      @submit.prevent="upload_csv" 
+      ref="csvFileUpload"
+      v-if="multipleUpload"
+    >
+      <div class="file_input">
+        <v-file-input
+          name="csv_employees"
+          placeholder="Click here to select a file..."
+          hint="Upload a CSV file to register multiple employees instead"
+          accept=".csv"
+          v-model="file"
+          :error-messages="fileUploadErrorMessage"
+          :error="fileUploadError"
+          persistent-hint
+          chips
+          :loading="uploadFileLoading"
+          show-size
+        />
 
-			<div class="field-group">
-				<div class="gender-field-group">
-					<label for="gender">Gender</label><br />
-					<input
-						type="radio"
-						name="gender"
-						value="true"
-						v-model="formData._isMale"
-					/>
-					Male
-					<input
-						type="radio"
-						name="gender"
-						value="false"
-						v-model="formData._isMale"
-					/>
-					Female
-				</div>
+        <v-btn 
+          color="#567DD8"
+          dark
+          class="file_upload_button_save"
+          type="submit"
+        >
+          Save
+        </v-btn>
+        <v-btn
+          color="#567DD8"
+          outlined
+          style="margin-left: 10px; width: 90px"
+          @click="closeDialog"
+        >
+          Cancel
+        </v-btn>
+      </div>
+    </v-form>
 
-				<div class="fingerprint-field-group">
-					<label for="fingerprint_number">Fingerprint Number</label><br />
-					<input
-						type="number"
-						name="fingerprint_number"
-						min="1"
-						v-model="formData.fingerprint_id"
-					/>
-				</div>
+    <v-divider />
 
-				<div class="email-field-group">
-					<label for="email">Email</label>
-					<input type="email" name="email" v-model="formData.email" />
-				</div>
-			</div>
+    <!-- FIXME: Change the form into vuetify's form -->
+    <!-- TODO: add the upload csv button for adding multiple employees -->
+    <v-form
+      class="addEmployeeForm"
+      method="POST"
+      v-if="!multipleUpload"
+      @submit.prevent="addEmployee"
+      ref="addEmployeeForm"
+      color="#567DD8"
+    >
+      <v-text-field
+        color="#567DD8"
+        class="form-input"
+        name="firstname"
+        v-model="firstname"
+        label="Firstname"
+        outlined
+        clearable
+        dense
+      />
 
-			<label for="employee_id">Employee ID</label>
-			<input type="text" name="employee_id" v-model="formData.employee_id" />
+      <v-text-field
+        color="#567DD8"
+        class="form-input"
+        name="lastname"
+        v-model="lastname"
+        label="Lastname"
+        outlined
+        clearable
+        dense
+      />
 
-			<label for="department">Department</label>
-			<input type="text" name="department" v-model="formData.department" />
+      <v-text-field
+        color="#567DD8"
+        class="form-input"
+        name="email"
+        v-model="email"
+        label="Email"
+        type="email"
+        outlined
+        clearable
+        dense
+      />
 
-			<label for="employment_status">Employment Status:</label>
-			<input
-				type="text"
-				name="employment_status"
-				v-model="formData.employment_status"
-			/>
+      <v-radio-group
+        class="form-input gender-input-group"
+        name="gender"
+        v-model="isMale"
+        dense
+        label="Gender"
+      >
+        <v-radio
+          class="gender-input"
+          label="Male"
+          :value="true"
+          color="#567DD8"
+        />
+        <v-radio
+          class="gender-input"
+          label="Female"
+          :value="false"
+          color="#567DD8"
+        />
+      </v-radio-group>
 
-			<label for="job_title">Job Title</label>
-			<input type="text" name="job_title" v-model="formData.job_title" />
+      <v-text-field
+        color="#567DD8"
+        class="form-input"
+        name="employee_id"
+        v-model="employee_id"
+        label="Employee ID"
+        outlined
+        clearable
+        dense
+      />
 
-			<div class="form-button-group">
-				<input class="form-button" type="submit" value="Save" />
-				<button
-					class="form-button closeButton"
-					@click="closeForm()"
-				>
-					Cancel
-				</button>
-			</div>
-		</form>
-	</div>
+      <v-combobox
+        color="#567DD8"
+        class="form-input"
+        :items="departmentCategories"
+        v-model="department"
+        label="Department"
+        outlined
+        dense
+      />
+
+      <v-text-field
+        color="#567DD8"
+        class="form-input"
+        name="job_title"
+        v-model="job_title"
+        label="Job Title"
+        outlined
+        clearable
+        dense
+      />
+
+      <v-combobox
+        color="#567DD8"
+        class="form-input"
+        v-model="employment_status"
+        :items="['Part-time', 'Full-time']"
+        label="Employment Status"
+        outlined
+        dense
+      />
+
+      <v-text-field
+        color="#567DD8"
+        class="form-input"
+        name="fingerprint_number"
+        v-model="fingerprint_id"
+        label="Fingerprint Number"
+        type="number"
+        outlined
+        clearable
+        dense
+      />
+
+      <div class="button_group">
+        <v-btn
+          color="#567DD8"
+          large 
+          dark
+          class="form_button"
+          type="submit"
+        >
+          Save
+        </v-btn>
+
+        <v-btn
+          color="#567DD8"
+          large
+          outlined
+          class="form_button"
+          @click="closeDialog"
+        >
+          Cancel
+        </v-btn>
+      </div>
+    </v-form>
+  </v-card>
 </template>
 
 <script>
 export default {
-	name: "AddEmployeeForm",
-	data: () => {
-		return {
-			userIsAdmin: true,
-			passwordHidden: false,
-			formData: {
-				firstname: "Nathaniel",
-				lastname: "Saludes",
-				_isMale: true,
-				fingerprint_id: 1,
-				email: "saludesnathaniel@gmail.com",
-				employee_id: "2015-02034",
-				department: "HR",
-				employment_status: 1,
-				job_title: "HR Assitant"
-			}
-		};
-	},
-	methods: {
-		closeForm() {
-			this.$emit("close");
-		},
-		addEmployee() {
-			this.$store.dispatch("employees/addEmployee", this.formData).then(() => {
-				this.closeForm();
-			});
-		}
-	}
+  name: "AddEmployeeForm",
+  data: () => {
+    return {
+      // sample form data ---------------------------------------------------
+      // firstname: "Nathaniel",
+      // lastname: "Saludes",
+      // email: "saludesnathaniel@gmail.com",
+      // isMale: true,
+      // fingerprint_id: 1,
+      // employee_id: "2015-02034",
+      // department: "HR",
+      // employment_status: null,
+      // job_title: "HR Assitant",
+      // --------------------------------------------------------------------
+
+      firstname        : null,
+      lastname         : null,
+      email            : null,
+      isMale           : null,
+      fingerprint_id   : null,
+      employee_id      : null,
+      department       : null,
+      employment_status: null,
+      job_title        : null,
+
+      // toggle CSV file upload form
+      multipleUpload: false,
+
+      // department categories
+      departmentCategories: ["Human Resources", "Finance", "Faculty", "IT Department"],
+
+      // csv upload
+      uploadFileLoading: false,
+      file: null,
+      fileUploadError: false,
+      fileUploadErrorMessage: ""
+    };
+  },
+  methods: {
+    addEmployee() {
+      // alert('addEmployee()');
+      this.$store.dispatch("employees/addEmployee", this.formData).then(() => {
+        this.closeDialog();
+      });
+    },
+    upload_csv() {
+      if (!this.file) {
+        this.fileUploadError = true;
+        this.fileUploadErrorMessage = "No file provided.";
+        return false;
+      }
+
+      // TODO: Implement HTTP request for CSV upload through store actions...
+      
+      return alert("upload_csv() not implemented!");
+    },
+    resetCSVFileUploadForm() {
+      this.$refs.csvFileUpload.reset();
+      this.uploadFileLoading      = false;
+      this.file                   = null;
+      this.fileUploadError        = false;
+      this.fileUploadErrorMessage = "";
+    },
+    closeDialog() {
+      if(this.multipleUpload) {
+        this.resetCSVFileUploadForm();
+        this.multipleUpload = false;
+        return this.$emit('closeDialog');
+      }
+
+      this.$refs.addEmployeeForm.reset();
+      this.$emit('closeDialog');
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-form {
-	// position: relative;
-	background-color: white;
-	color: #656565;
-	padding: 28px 25px;
-	box-shadow: 4px 6px 4px 4px #00000025;
-	display: flex;
-	flex-direction: column;
+.form_container {
+  display: flex;
+  flex-direction: column;
 }
 
-h1 {
-	margin-bottom: 10px;
-	color: #779aec;
+.fileUploadToggleButton {
+  position: absolute;
+  right: 10px;
 }
 
-.close-button {
-	position: absolute;
-	z-index: 3;
-	right: 668px;
-	top: 135px;
-	height: 12px;
+.file_input {
+  width: 93%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0px 0px 10px 20px;
+
+  .file_upload_button_save {
+    margin-left: 20px;
+  }
 }
 
-.form-graphics {
-	z-index: 2;
-	height: 150px;
-	position: absolute;
-	top: 50px;
-	left: 850px;
+.addEmployeeForm {
+  background-color: white;
+  color: #656565;
+  padding: 28px 25px;
+  box-shadow: 4px 6px 4px 4px #00000025;
+  display: flex;
+  flex-direction: column;
+
+  .form-input {
+    padding: 0px !important;
+    margin: 0px;
+  }
+
+  .button_group {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+
+    .form_button {
+      width: 150px;
+
+      &:first-child {
+        margin-right: 10px;
+      }
+    }
+  }
 }
 
-label {
-	font-family: "Cera Pro", sans-serif;
-	font-size: 13px;
-	margin-bottom: 2px;
+.hideFileUpload {
+  display: none;
 }
 
-input[type="text"],
-input[type="email"],
-input[type="number"] {
-	padding: 10px;
-	border: 1px solid #e1e1e1;
-	border-radius: 5px;
-	margin-bottom: 10px;
-}
+// .gender-input-group >>> .v-input--radio-group__input {
+// 	display: flex;
+// 	flex-direction: row !important;
+// 	width: 50%;
+// 	background-color: red;
 
-.field-group {
-	display: flex;
-	flex-direction: row;
-	// background-color: blue;
-
-	.fingerprint-field-group {
-		// background-color: red;
-		width: 150px;
-		margin-right: 15px;
-		input {
-			width: 100%;
-		}
-	}
-
-	.email-field-group {
-		flex-grow: 1;
-		// background-color: red;
-
-		input {
-			width: 100%;
-		}
-	}
-
-	.gender-field-group {
-		width: 140px;
-	}
-}
-
-.form-button-group {
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	justify-content: flex-end;
-	margin-top: 5px;
-	// background-color: blue;
-
-	.form-button {
-		padding: 10px;
-		font-family: "Product Sans Medium", sans-serif;
-		font-size: 16px;
-	}
-
-	input[type="submit"] {
-		background-color: #779aec;
-		color: white;
-		border-radius: 5px;
-		width: 130px;
-		margin-right: 10px;
-	}
-
-	.closeButton {
-		background-color: transparent;
-		color: #d8d8d8;
-		border: 1px solid #e1e1e1;
-		border-radius: 5px;
-		width: 130px;
-	}
-}
+// 	.gender-input {
+// 		background-color: aqua;
+// 		display: flex;
+// 		width: 50%;
+// 		padding: 0px;
+// 		margin: 0px;
+// 	}
+// }
 </style>
