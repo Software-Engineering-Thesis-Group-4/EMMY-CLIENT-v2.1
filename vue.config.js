4676967102ea457d7ba1bdb3a2436b01adfa47fc
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-04-10 16:46:16 +0800

    changed the output directory configuration of the built files.

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,16 @@
 const path = require('path');
+const ip = require('ip');
 
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  outputDir: path.resolve(__dirname, '../EMMY-SERVER/client'),
-  devServer: {
-    proxy: {
-      '/api': {
-        target: `http://localhost:3000`
-      }
-    }
-  }
+	"transpileDependencies": [
+		"vuetify"
+	],
+	outputDir: path.resolve(__dirname, '../EMMY-SERVER-v2.0/client'),
+	devServer: {
+		proxy: {
+			'/api': {
+				target: `http://${ip.address()}:3000`
+			}
+		}
+	}
 }
