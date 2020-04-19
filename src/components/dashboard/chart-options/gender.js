16bd175c0958443683122958d164d00d6fe45918
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-04-19 14:42:15 +0800

    removed global chart styles

--- src/components/dashboard/chart-options/gender.js
+++ src/components/dashboard/chart-options/gender.js
@@ -1,10 +1,12 @@
 
-// Global chart options for gender charts
-window.Apex = {
+export const maleChart = {
+	data: [84, 97, 10, 17, 35, 69],
 	chart: {
 		// background: '#ff00004d',
-		width: '100%',
+		id: 'male',
+		group: 'genderChart',
 		type: 'donut',
+		width: '100%',
 	},
 	colors: ["#FF8484", "#FFB546", "#FFF23B", "#A8FF84", "#65FBDC", "#B2BEC3"],
 	labels: ["Angry", "Sad", "Okay", "Happy", "Amazing", "Unsubmitted"],
@@ -82,31 +84,103 @@ window.Apex = {
 				}
 			}
 		},
-	}
-}
-
-export const maleChart = {
-	data: [84, 97, 10, 17, 35, 69],
-	chart: {
-		id: 'male',
-		group: 'genderChart',
-		type: 'donut',
+	},
+	yaxis: {
+		labels: {
+			minWidth: '150px'
+		}
 	}
 }
 
 export const femaleChart = {
 	data: [68, 27, 90, 59, 31, 16],
 	chart: {
+		// background: '#ff00004d',
 		id: 'female',
 		group: 'genderChart',
 		type: 'donut',
-	}
-}
-
-export const chartOptions = {
-
+		width: '100%',
+	},
+	colors: ["#FF8484", "#FFB546", "#FFF23B", "#A8FF84", "#65FBDC", "#B2BEC3"],
+	labels: ["Angry", "Sad", "Okay", "Happy", "Amazing", "Unsubmitted"],
 	dataLabels: {
 		enabled: false,
 	},
-
+	tooltip: {
+		enabled: true,
+	},
+	legend: {
+		show: true,
+		showForSingleSeries: false,
+		showForNullSeries: true,
+		showForZeroSeries: true,
+		position: 'bottom',
+		horizontalAlign: 'center',
+		floating: false,
+		fontSize: '14px',
+		fontFamily: 'Cera Pro, Arial',
+		fontWeight: 400,
+		height: undefined,
+		offsetX: 0,
+		offsetY: 5,
+		labels: {
+			colors: undefined,
+			useSeriesColors: false
+		},
+	},
+	noData: {
+		text: "Loading data...",
+		align: "center",
+		verticalAlign: "middle",
+		offsetX: 0,
+		offsetY: 0,
+		style: {
+			color: "#779AEC",
+			fontSize: "15px",
+			fontFamily: "Cera Pro"
+		}
+	},
+	plotOptions: {
+		pie: {
+			expandOnClick: false,
+			// offsetY: 10,
+			donut: {
+				size: '65%',
+				labels: {
+					show: true,
+					name: {
+						show: true,
+						fontSize: '15px',
+						fontFamily: 'Cera Pro, Arial',
+						color: 'black',
+					},
+					total: {
+						show: true,
+						showAlways: false,
+						label: 'Total',
+						fontSize: '22px',
+						fontFamily: 'Cera Pro, Arial',
+						fontWeight: 600,
+						color: '#373d3f',
+					},
+					value: {
+						show: true,
+						fontSize: '18px',
+						fontFamily: 'Cera Pro, Arial, sans-serif',
+						fontWeight: 600,
+						color: undefined,
+						offsetY: 0,
+						formatter: function (val) {
+							return val
+						}
+					},
+				}
+			}
+		},
+	},
+	yaxis: {
+		labels: {
+			minWidth: '150px'
+		}
+	}
 }
