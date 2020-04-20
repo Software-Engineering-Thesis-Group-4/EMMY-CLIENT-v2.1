cc6d86ffc882e041814240a123911e299cdb3720
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-04-20 14:08:28 +0800

    fixed indentations

--- src/views/layout-admin-login.vue
+++ src/views/layout-admin-login.vue
@@ -1,14 +1,29 @@
 <template>
 	<div class="root">
 		<v-container class="root-container">
-			<v-card class="form-container" :elevation="2">
+			<v-card
+				class="form-container"
+				:elevation="2"
+			>
 				<div class="logo-container">
-					<img src="/logo/logo-dark@2x.png" class="logo" alt srcset />
+					<img
+						src="/logo/logo-dark@2x.png"
+						class="logo"
+						alt
+						srcset
+					/>
 				</div>
 
-				<form class="login-form" @submit.prevent="login()">
+				<form
+					class="login-form"
+					@submit.prevent="login()"
+				>
 					<div class="field">
-						<img src="@/assets/images/input-field-icon_user.svg" alt srcset />
+						<img
+							src="@/assets/images/input-field-icon_user.svg"
+							alt
+							srcset
+						/>
 						<input
 							type="text"
 							name="email"
@@ -31,21 +46,32 @@
 						/>
 					</div>
 
-					<div v-if="errorMessage" class="login-error-message">
+					<div
+						v-if="errorMessage"
+						class="login-error-message"
+					>
 						{{ errorMessage }}
 					</div>
 
-					<input type="submit" value="LOGIN" class="login-button" />
-					<router-link to="/forgotpassword" class="forgot-password"
-						>Forgot your password?</router-link
-					>
+					<input
+						type="submit"
+						value="LOGIN"
+						class="login-button"
+					/>
+					<router-link
+						to="/forgotpassword"
+						class="forgot-password"
+					>Forgot your password?</router-link>
 				</form>
 				<div class="no-account">
 					Don't have an account? Please contact your administrator
 				</div>
 			</v-card>
 
-			<a href="https://iacademy.edu.ph" class="iacademy-link">
+			<a
+				href="https://iacademy.edu.ph"
+				class="iacademy-link"
+			>
 				<img
 					class="iacademy"
 					src="@/assets/images/iACADEMY_Seal.svg"
