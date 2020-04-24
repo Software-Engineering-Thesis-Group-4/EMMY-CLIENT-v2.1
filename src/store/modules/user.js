0bcbf86cb8de4b2caeaf17c0241f5cf1a5b2b438
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-04-25 06:28:53 +0800

    added logout action. added additional user details (firstname, lastname and photo).

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,60 +1,102 @@
 import Vue from 'vue'
+import moment from 'moment';
 
 const UserModule = {
 	namespaced: true,
 	state: {
 		error: null,
 		username: null,
+		firstname: null,
+		lastname: null,
 		email: null,
 		isAdmin: false,
+		photo: null,
 	},
 	mutations: {
 		AUTH_ERROR(state, message) {
 			state.error = message;
 			localStorage.clear();
 		},
-		AUTH_SUCCESS(state, payload) {
-			state.error    = "";
-			state.username = payload.username;
-			state.email    = payload.email;
-			state.isAdmin  = payload.isAdmin
-
-			// store access token in localStorage
-			localStorage.setItem('access_token', payload.token);
+		AUTH_SUCCESS(state, user) {
+			state.error = "";
+			state.username = user.username;
+			state.email = user.email;
+			state.isAdmin = user.isAdmin;
+			state.firstname = user.firstname;
+			state.lastname = user.lastname;
+			state.photo = user.photo;
 		},
 		CLEAR(state) {
 			state.error = null;
 			state.username = null;
+			state.firstname = null;
+			state.lastname = null;
 			state.email = null;
 			state.isAdmin = false;
-			localStorage.clear();
+			state.photo = null;
+
+			localStorage.removeItem('access_token');
 		}
 	},
 	actions: {
 		// TODO: Implement fetching of users
-		// async FETCH_USERS(context) {
-		// 	try {
-		// 		let response = await Vue.axios.get('/api/')
-		// 	} catch (error) {
+		/* async FETCH_USERS(context) {
+			try {
+				let response = await Vue.axios.get('/api/')
+			} catch (error) {
 				
-		// 	}
-		// },
+			}
+		}, */
 		async LOGIN(context, { email, password }) {
 			try {
 
-				let response = await Vue.axios.post('/auth/login', { email, password });
+				const response = await Vue.axios.post('/auth/login', { email, password });
 				context.commit('AUTH_SUCCESS', response.data);
+
+				// store access token in localStorage
+				localStorage.setItem('access_token', response.data.token);
+
+				console.log(`%c Login Success.`,
+					`
+					color: limegreen; 
+					font-family: "Cera Pro", sans-serif;
+					font-size: 14px;
+				`
+				);
 
 				return true;
 
 			} catch (error) {
-				console.log(error.response.data);
+				console.log(`%c Login Failed. (${moment().format('LTS')})`,
+					`
+						color: red; 
+						font-family: "Cera Pro", sans-serif;
+						font-size: 14px;
+					`
+				);
 				context.commit('AUTH_ERROR', error.response.data);
 				return false;
 			}
 		},
-		LOGOUT() {
+		async LOGOUT({ commit, state }) {
+			try {
+				let response = await Vue.axios.post('/auth/logout', { email: state.email });
+				if (response.status === 200) {
+					localStorage.getItem('access_token')
+				}
+				commit('CLEAR');
+				return true;
 
+			} catch (error) {
+				console.log(`%c Logout Error. ${error.response.data}. (${moment().format('LTS')})`,
+					`
+						color: red; 
+						font-family: "Cera Pro", sans-serif;
+						font-size: 14px;
+					`
+				);
+				return false;
+			}
 		},
 		async VERIFY({ commit, state }) {
 			try {
@@ -70,7 +112,7 @@ const UserModule = {
 				return false;
 
 			} catch (error) {
-				console.log(error.response.data);
+				console.log(`%c ${error.response.data} (${moment().format('LTS')})`, 'color:red; font-family: "Cera Pro", sans-serif; font-size: 14px;');
 				commit('CLEAR');
 				return false;
 			}
@@ -78,6 +120,7 @@ const UserModule = {
 	},
 	getters: {
 		errorMessage: state => state.error,
+		fullname: state => `${state.firstname} ${state.lastname}`,
 		username: state => state.username,
 		email: state => state.email,
 	}
