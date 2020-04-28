91f1e2a9f54b929a3aa1db24b1488bfc2ae07a74
dineros <projectdineros@gmail.com>
2020-04-29 00:28:10 +0800

    added  the data table

--- src/views/layout-admin-logs.vue
+++ src/views/layout-admin-logs.vue
@@ -1,12 +1,110 @@
 <template>
   <div>
     <v-divider color="#52555A"></v-divider>
+    <div class="table-container">
+      <template>
+        <v-data-table dark
+                      :headers="headers" 
+                      :items="employees" 
+                      :items-per-page="10" 
+                      class="logs-table"></v-data-table>
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+    data () {
+      return {
+        headers: [
+          {
+            text: 'Date',
+            align: 'start',
+            sortable: false,
+            value: 'date',
+          },
+          { text: 'Time', value: 'time' },
+          { text: 'Description', value: 'description' },
+          { text: 'Employee', value: 'employee' }
+        ],
+        employees: [
+         {
+          date: "Jan 1, 2020",
+          time: "hh:mm:ss",
+          description:
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+          employee: "Dolores, Ipsum Lorem"
+        },
+        {
+          date: "Jan 2, 2020",
+          time: "hh:mm:ss",
+          description:
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+          employee: "Dolores, Ipsum Lorem"
+        },
+        {
+          date: "Jan 3, 2020",
+          time: "hh:mm:ss",
+          description:
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+          employee: "Dolores, Ipsum Lorem"
+        },
+        {
+          date: "Jan 4, 2020",
+          time: "hh:mm:ss",
+          description:
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+          employee: "Dolores, Ipsum Lorem"
+        },
+        {
+          date: "Jan 5, 2020",
+          time: "hh:mm:ss",
+          description:
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+          employee: "Dolores, Ipsum Lorem"
+        },
+        {
+          date: "Jan 6, 2020",
+          time: "hh:mm:ss",
+          description:
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+          employee: "Dolores, Ipsum Lorem"
+        },
+        {
+          date: "Jan 7, 2020",
+          time: "hh:mm:ss",
+          description:
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+          employee: "Dolores, Ipsum Lorem"
+        },
+        {
+          date: "Jan 8, 2020",
+          time: "hh:mm:ss",
+          description:
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+          employee: "Dolores, Ipsum Lorem"
+        },
+        {
+          date: "Jan 9, 2020",
+          time: "hh:mm:ss",
+          description:
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+          employee: "Dolores, Ipsum Lorem"
+        },
+        {
+          date: "Jan 10, 2020",
+          time: "hh:mm:ss",
+          description:
+            "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+          employee: "Dolores, Ipsum Lorem"
+        }
+      ],
+      }
+    },
+  }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  
 </style>
