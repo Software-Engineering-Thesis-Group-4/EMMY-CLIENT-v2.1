76a941784b632987f5b5ce218da1235072023017
dineros <projectdineros@gmail.com>
2020-04-29 00:31:00 +0800

    fixing the timepicker and drop down button

--- src/views/layout-admin-config.vue
+++ src/views/layout-admin-config.vue
@@ -5,18 +5,57 @@
     <div class="backup-settings">
       <h2 class="group-label">Backup Settings</h2>
 
-      <div class="time-field">
-        <div class="frequency-label">Time</div>
-        <button class="button-time">
-          <span>12:00 A.M.</span>
-        </button>
+      <!-- Timepicker Field --------------------------------------------------------------------------------------->
+      <div class="timepicker-field">
+        <div class="time-label">Time</div>
+        <v-menu
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+          min-width="290px"
+        >
+          <template v-slot:activator="{ on }">
+            <v-button class="button-time" v-on="on">
+              <span>{{ time }}</span>
+              <v-icon class="button-icon-right">mdi-chevron-down</v-icon>
+            </v-button>
+          </template>
+
+          <v-time-picker 
+           class="time-picker"
+           dark
+           v-model="time" 
+           ampm-in-title="ampmInTitle"           
+           @change="$refs.menu.save(time)">
+            <v-spacer></v-spacer>
+            <v-btn color="#6f7276"  @click="dropdownOpen= false">Cancel</v-btn>
+            <v-btn color="#6f7276" @click="$refs.dialog.save(time)">OK</v-btn>
+          </v-time-picker>
+        </v-menu>
       </div>
+
       <div class="frequency-field">
         <div class="frequency-label">Frequency</div>
-        <button class="button-time">
+
+        <v-combobox 
+          color="#5f7bbe"
+          class="frequency-combobox"
+          label="Hours"
+          :items="['Every 12 Hours', ' Every 24 Hours']"
+          min-width="290px"
+          
+          outlined
+          dense
+          clearable
+          @change="timeFormat"
+        ></v-combobox>
+
+        <!-- <v-button class="button-frequency">
           <span>24 Hours</span>
-        </button>
+          <v-icon class="button-icon-right">mdi-chevron-down</v-icon>
+        </v-button> -->
       </div>
+
       <div>
         <v-btn class="save-button" color="#333737">
           <span>Save</span>
@@ -24,16 +63,8 @@
       </div>
     </div>
 
-    <!-- Time Picker -->
-    <!-- <v-time-picker 
-        class="time-picker" 
-        v-model="time" 
-        @change="$refs.menu.save(time)">
-    </v-time-picker>-->
-    
-    <v-divider color="#52555A"></v-divider>
-
     <!-- Database Backup  -->
+    <v-divider color="#52555A"></v-divider>
     <h2 class="group-label">Database Backup</h2>
     <div>
       <v-btn class="download-button" color="#82efca">
@@ -53,9 +84,20 @@
 <script>
 export default {
   data() {
+      
     return {
-      time: null
-    };
+        time: null,
+        menu2: false,
+        modal2: false
+      }
+  // return {
+  // timeFormat: 'hh:mm a', 
+  // Data: {
+  //   hh: '03',
+  //   mm: '05',
+  //   a: 'A.M.'
+  // } create computed formula for AM & PM
+
   }
 };
 </script>
@@ -63,18 +105,14 @@ export default {
 <style lang="scss" scoped>
 .page-header {
   color: #707070;
+  letter-spacing: 1px;
 }
 
 .button-time {
   background-color: #202224;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
   height: 40px;
-  padding: 5px 9px 5px 15px;
-  margin-left: 10px;
+  padding: 10px 24px 10px 24px;
+  margin-left: 51px;
   font-weight: 500;
   color: #494c4f;
 
@@ -83,6 +121,30 @@ export default {
   }
 }
 
+.button-frequency {
+  background-color: #202224;
+  height: 40px;
+  padding: 10px 24px 10px 24px;
+  margin-left: 10px;
+  font-weight: 500;
+
+  &:hover {
+    filter: brightness(0.95);
+  }
+}
+
+.button-icon-right {
+  font-size: 18px;
+  margin-left: 8px;
+  color: white;
+  opacity: 0.6;
+}
+
+.timepicker-field {
+  display: flex;
+}
+
+
 .time-field {
   display: flex;
   color: #494c4f;
@@ -90,7 +152,9 @@ export default {
 }
 
 .time-label {
-  margin: 7px 0px 0px 10px;
+  margin: 7px 10px 0px 0px;
+  color: #494c4f;
+  //background-color: blue;
 }
 
 .frequency-field {
@@ -99,8 +163,13 @@ export default {
   margin: 10px 0px 20px 0px;
 }
 
+.frequency-combobox {
+    margin: 0px 20px 0px 0px;
+    width: 20%;
+}
+
 .frequency-label {
-  margin: 8px 0px 0px 0px;
+  margin: 8px 20px 10px 0px;
 }
 
 .group-label {
@@ -109,20 +178,20 @@ export default {
 }
 
 .save-button {
-    width: 20%;
-    border-radius: 5px;
-    border: none;
-    padding: 15px;
-    color: #6F7276;
-    font-size: 14px;
-    margin: 0px 0px 50px 0px;
-    text-transform: none;
+  width: 20%;
+  border-radius: 5px;
+  border: none;
+  padding: 15px;
+  color: #6f7276;
+  font-size: 14px;
+  margin: 0px 0px 50px 0px;
+  text-transform: none;
 
-    &:hover {
-      cursor: pointer;
-      background-color: #8bffd8;
-    }
+  &:hover {
+    cursor: pointer;
+    background-color: #8bffd8;
   }
+}
 
 .download-button {
   width: 20%;
@@ -157,10 +226,7 @@ export default {
 }
 
 .backup-settings {
-  display: flexbox;
+  display: flex;
+  flex-direction: column;
 }
-
-// .time-picker {
-//     margin: 20px 0px 20px 0px;
-// }
 </style>
