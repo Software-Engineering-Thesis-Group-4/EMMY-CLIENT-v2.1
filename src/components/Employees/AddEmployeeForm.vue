fc2f269560850143e071990397b153b4d68202e2
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-05-05 09:37:47 +0800

    readded Employees folder (fixed casing)

---
+++ src/components/Employees/AddEmployeeForm.vue
@@ -0,0 +1,408 @@
+<template>
+	<v-card class="form_container">
+		<v-toolbar
+			flat
+			dense
+			dark
+			color="#567DD8"
+			short
+			min-width="100%"
+		>
+			<span>Register Employees</span>
+
+			<v-btn
+				class="fileUploadToggleButton"
+				small
+				dark
+				outlined
+				@click="toggleFileUpload"
+			>
+				<span v-if="multipleUpload">&lt; Back</span>
+				<span v-else>Import CSV</span>
+			</v-btn>
+		</v-toolbar>
+
+		<!-- <p class="upload_file-label">Upload a CSV file:</p> -->
+		<v-form
+			@submit.prevent="upload_csv"
+			ref="csvFileUpload"
+			v-if="multipleUpload"
+		>
+			<div class="file_input">
+				<v-file-input
+					name="csv_employees"
+					placeholder="Click here to select a file..."
+					hint="Upload a CSV file to register multiple employees instead"
+					accept=".csv"
+					v-model="file"
+					:error-messages="fileUploadErrorMessage"
+					:error="fileUploadError"
+					persistent-hint
+					chips
+					:loading="uploadFileLoading"
+					show-size
+				/>
+
+				<v-btn
+					color="#567DD8"
+					dark
+					class="file_upload_button_save"
+					type="submit"
+				>Save</v-btn>
+				<v-btn
+					color="#567DD8"
+					outlined
+					style="margin-left: 10px; width: 90px"
+					@click="closeDialog"
+				>Cancel</v-btn>
+			</div>
+		</v-form>
+
+		<v-divider />
+
+		<!-- FIXME: Change the form into vuetify's form -->
+		<!-- TODO: add the upload csv button for adding multiple employees -->
+		<v-form
+			class="addEmployeeForm"
+			method="POST"
+			v-if="!multipleUpload"
+			@submit.prevent="addEmployee"
+			ref="addEmployeeForm"
+			color="#567DD8"
+		>
+			<v-text-field
+				color="#567DD8"
+				class="form-input"
+				name="firstname"
+				v-model="firstname"
+				label="Firstname"
+				outlined
+				clearable
+				dense
+			/>
+
+			<v-text-field
+				color="#567DD8"
+				class="form-input"
+				name="lastname"
+				v-model="lastname"
+				label="Lastname"
+				outlined
+				clearable
+				dense
+			/>
+
+			<v-text-field
+				color="#567DD8"
+				class="form-input"
+				name="email"
+				v-model="email"
+				label="Email"
+				type="email"
+				outlined
+				clearable
+				dense
+			/>
+
+			<v-radio-group
+				class="form-input gender-input-group"
+				name="gender"
+				v-model="isMale"
+				dense
+				label="Gender"
+			>
+				<v-radio
+					class="gender-input"
+					label="Male"
+					:value="true"
+					color="#567DD8"
+				/>
+				<v-radio
+					class="gender-input"
+					label="Female"
+					:value="false"
+					color="#567DD8"
+				/>
+			</v-radio-group>
+
+			<v-text-field
+				color="#567DD8"
+				class="form-input"
+				name="employee_id"
+				v-model="employee_id"
+				label="Employee ID"
+				outlined
+				clearable
+				dense
+			/>
+
+			<v-combobox
+				color="#567DD8"
+				class="form-input"
+				:items="departmentCategories"
+				v-model="department"
+				label="Department"
+				outlined
+				dense
+			/>
+
+			<v-text-field
+				color="#567DD8"
+				class="form-input"
+				name="job_title"
+				v-model="job_title"
+				label="Job Title"
+				outlined
+				clearable
+				dense
+			/>
+
+			<v-combobox
+				color="#567DD8"
+				class="form-input"
+				v-model="employment_status"
+				:items="['Part-time', 'Full-time']"
+				label="Employment Status"
+				outlined
+				dense
+			/>
+
+			<v-text-field
+				color="#567DD8"
+				class="form-input"
+				name="fingerprint_number"
+				v-model="fingerprint_id"
+				label="Fingerprint Number"
+				type="number"
+				outlined
+				clearable
+				dense
+			/>
+
+			<div class="button_group">
+				<v-btn
+					color="#567DD8"
+					large
+					dark
+					class="form_button"
+					type="submit"
+				>Save</v-btn>
+
+				<v-btn
+					color="#567DD8"
+					large
+					outlined
+					class="form_button"
+					@click="closeDialog"
+				>Cancel</v-btn>
+			</div>
+		</v-form>
+	</v-card>
+</template>
+
+<script>
+export default {
+	name: "AddEmployeeForm",
+	data: () => {
+		return {
+			// firstname: null,
+			// lastname: null,
+			// email: null,
+			// isMale: null,
+			// fingerprint_id: null,
+			// employee_id: null,
+			// department: null,
+			// employment_status: null,
+			// job_title: null,
+
+			firstname: "John",
+			lastname: "Doe",
+			email: "johndoe@gmail.com",
+			isMale: true,
+			fingerprint_id: 55,
+			employee_id: "2020-02056",
+			department: "Purchasing",
+			employment_status: "Full-time",
+			job_title: "jobless",
+
+			// toggle CSV file upload form
+			multipleUpload: false,
+
+			// department categories
+			departmentCategories: [
+				"Admissions",
+				"Registrar",
+				"Finance",
+				"Human Resources ",
+				"Office of Student Affairs",
+				"Office of Student Experience and Advancement ",
+				"Office of the President",
+				"Office of the COO",
+				"IT",
+				"Corporate Communications",
+				"Purchasing",
+				"Admin and Facilities",
+				"Academics College",
+				"Academics SHS",
+				"Clinic"
+			],
+
+			// csv upload
+			uploadFileLoading: false,
+			file: null,
+			fileUploadError: false,
+			fileUploadErrorMessage: ""
+		};
+	},
+	methods: {
+		addEmployee() {
+			// FIX: Remove this upon pushing project into production mode...
+			console.log({
+				firstname: this.firstname,
+				lastname: this.lastname,
+				email: this.email,
+				isMale: this.isMale,
+				fingerprint_id: this.fingerprint_id,
+				employee_id: this.employee_id,
+				department: this.department,
+				employment_status: this.employment_status,
+				job_title: this.job_title
+			});
+
+			let formData = {
+				firstname: this.firstname,
+				lastname: this.lastname,
+				email: this.email,
+				isMale: this.isMale,
+				fingerprint_id: this.fingerprint_id,
+				employee_id: this.employee_id,
+				department: this.department,
+				employment_status: this.employment_status,
+				job_title: this.job_title
+			};
+
+			this.$store
+				.dispatch("employees/ADD_EMPLOYEE", formData)
+				.then(() => {
+					this.$emit('addedNewEmployee');
+					this.closeDialog();
+				});
+		},
+		upload_csv() {
+			if (!this.file) {
+				this.fileUploadError = true;
+				this.fileUploadErrorMessage = "No file provided.";
+				return false;
+			}
+
+			// TODO: Implement HTTP request for CSV upload
+			this.$http.get('')
+
+			return alert("upload_csv() not implemented!");
+		},
+		resetFormData() {
+			this.firstname = null;
+			this.lastname = null;
+			this.email = null;
+			this.isMale = null;
+			this.fingerprint_id = null;
+			this.employee_id = null;
+			this.department = null;
+			this.employment_status = null;
+			this.job_title = null;
+		},
+		resetCSVFileUploadForm() {
+			this.$refs.csvFileUpload.reset();
+			this.uploadFileLoading = false;
+			this.file = null;
+			this.fileUploadError = false;
+			this.fileUploadErrorMessage = "";
+		},
+		closeDialog() {
+			if (this.multipleUpload) {
+				this.resetCSVFileUploadForm();
+				this.resetFormData();
+				this.multipleUpload = false;
+				return this.$emit("closeDialog");
+			}
+
+			this.$refs.addEmployeeForm.reset();
+			this.$emit("closeDialog");
+		},
+		toggleFileUpload() {
+			this.multipleUpload = !this.multipleUpload;
+		}
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.form_container {
+	display: flex;
+	flex-direction: column;
+}
+
+.fileUploadToggleButton {
+	position: absolute;
+	right: 10px;
+}
+
+.file_input {
+	width: 93%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: 0px 0px 10px 20px;
+
+	.file_upload_button_save {
+		margin-left: 20px;
+	}
+}
+
+.addEmployeeForm {
+	background-color: white;
+	color: #656565;
+	padding: 28px 25px;
+	box-shadow: 4px 6px 4px 4px #00000025;
+	display: flex;
+	flex-direction: column;
+
+	.form-input {
+		padding: 0px !important;
+		margin: 0px;
+	}
+
+	.button_group {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-end;
+
+		.form_button {
+			width: 150px;
+
+			&:first-child {
+				margin-right: 10px;
+			}
+		}
+	}
+}
+
+.hideFileUpload {
+	display: none;
+}
+
+// .gender-input-group >>> .v-input--radio-group__input {
+// 	display: flex;
+// 	flex-direction: row !important;
+// 	width: 50%;
+// 	background-color: red;
+
+// 	.gender-input {
+// 		background-color: aqua;
+// 		display: flex;
+// 		width: 50%;
+// 		padding: 0px;
+// 		margin: 0px;
+// 	}
+// }
+</style>
---
+++ src/components/Employees/DataTable/options.js
@@ -0,0 +1,73 @@
+// this function is called in the employee-list vue component to load all the employees OR update the list of employees
+export function loadTableData(employees) {
+	let employeeTableData = [];
+
+	if (!employees) {
+		return false;
+	}
+
+	employees.forEach(employee => {
+		let employmentStatus = (employee.employmentStatus ? 'Full-time Employee' : 'Part-time Employee');
+		if (!employee.terminated) {
+			employeeTableData.push({
+				id               : employee._id,
+				employeeId       : employee.employeeId,
+				firstname        : employee.firstName,
+				lastname         : employee.lastName,
+				name             : `${employee.lastName}, ${employee.firstName}`,
+				email            : employee.email,
+				department       : employee.department,
+				jobTitle         : employee.jobTitle,
+				employmentStatus : employmentStatus,
+			});
+		}
+	});
+
+	options.data = employeeTableData;
+
+	return true;
+}
+
+export function getData() {
+	return options.data;
+}
+
+export let options = {
+	search: null,
+	selected: [],
+	headers: [
+		{
+			text: "EMPLOYEE",
+			value: "name",
+			align: "left",
+			sortable: true,
+		},
+		{
+			text: "EMAIL",
+			value: "email",
+			sortable: true
+		},
+		{
+			text: "DEPARTMENT",
+			value: "department",
+			sortable: true
+		},
+		{
+			text: "JOB TITLE",
+			value: "jobTitle",
+			sortable: true
+		},
+		{
+			text: "EMPLOYMENT STATUS",
+			value: "employmentStatus",
+			sortable: true,
+		},
+		{
+			text: "EDIT / DELETE",
+			align: "center",
+			value: "actions",
+			sortable: false,
+		}
+	],
+	data: [],
+}
