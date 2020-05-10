7741bbce1dd4f3d03403f02165fcb16c9681642c
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-05-11 06:56:45 +0800

    added controls and datatable component placeholder

--- src/views/layout-admin-users.vue
+++ src/views/layout-admin-users.vue
@@ -1,5 +1,37 @@
 <template>
 	<div class="layout-container">
+
+		<div class="controls">
+			<!-- Search -->
+			<div class="controls__search">
+				<v-autocomplete
+					clearable
+					hide-no-data
+					color="#779AEC"
+					label="Search User..."
+					prepend-icon="mdi-database-search"
+				></v-autocomplete>
+			</div>
+
+			<!-- Add User -->
+			<v-dialog
+				max-width="600px"
+				v-model="addUser"
+			>
+				<template v-slot:activator="{ on }">
+					<button
+						class="controls__add_user"
+						v-on="on"
+					>
+						<v-icon class="add_user__icon">mdi-plus</v-icon>Add New User
+					</button>
+				</template>
+			</v-dialog>
+		</div>
+
+		<div class="users_container">
+			User Accounts List (Component)
+		</div>
 	</div>
 </template>
 
@@ -7,7 +39,8 @@
 export default {
 	data() {
 		return {
-			loadingTable: false
+			loadingTable: false,
+			addUser: false
 		};
 	},
 	computed: {},
@@ -17,4 +50,70 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.layout-container {
+	// background-color: rgba(64, 224, 208, 0.616);
+	// border: 1px dashed lightslategray;
+	width: 100%;
+}
+
+.controls {
+	// FOR DEBUGGING ---------------------------------------------
+	// background-color: #00ffff42;
+	// border: 1px dashed #0000003a;
+	// -----------------------------------------------------------
+	display: flex;
+	align-items: center;
+	width: 100%;
+	flex-wrap: wrap;
+}
+
+.controls__search {
+	// background-color: #ff00002d;
+	// border: 1px dashed lightslategray;
+	display: flex;
+	align-items: flex-start;
+	height: 58px;
+	width: 400px;
+}
+
+.controls__add_user {
+	background-color: #7198f3;
+	background: linear-gradient(0deg, #5a79c2 0%, #7198f3 100%);
+
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	height: 40px;
+
+	padding: 5px 15px;
+	margin-left: 20px;
+
+	font-weight: 500;
+	color: white;
+
+	border: 1.5px solid #5f7bbe;
+	border-radius: 5px;
+	transition: filter 0.1s ease;
+
+	&:hover {
+		filter: brightness(0.95);
+	}
+
+	.add_user__icon {
+		font-size: 16px;
+		margin-right: 5px;
+		margin-left: -5px;
+		color: white;
+		opacity: 0.6;
+	}
+}
+
+.users_container {
+	background-color: #ff00ff1c;
+	border: 1px dashed #0000002c;
+	min-height: 500px;
+	color: #9e009e73;
+	margin-top: 10px;
+}
 </style>
