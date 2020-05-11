cab76ff8dff534f0b95dbf1838497c4b16a63084
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-05-11 16:49:30 +0800

    removed ip package

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,8 @@
 const path = require('path');
-const ip = require('ip');
 
 module.exports = {
 	"transpileDependencies": [
 		"vuetify"
 	],
-	outputDir: path.resolve(__dirname, '../EMMY-SERVER/client'),
-	// devServer: {
-	// 	proxy: {
-	// 		'/api': {
-	// 			target: `http://${ip.address()}:3000`,
-	// 		},
-	// 		/* '/foo': {
-	// 			target: '<url>'
-	// 		} */
-	// 	}
-	// }
+	outputDir: path.resolve(__dirname, '../EMMY-SERVER/client')
 }
