b705f2bee1ccf37b1f0c083c40506c4bf515eb12
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-05-26 13:50:47 +0800

    fetch and render audit logs form server

--- src/views/layout-admin-logs.vue
+++ src/views/layout-admin-logs.vue
@@ -3,11 +3,26 @@
 		<div class="table-container">
 			<template>
 				<v-data-table
-					:headers="headers"
-					:items="employees"
+					:headers="options.headers"
+					:items="options.data"
 					:items-per-page="10"
+					:loading="loading"
 					class="logs-table"
 				>
+
+					<template #item.date="{item, value}">
+						{{ moment__parseDate(value) }}
+					</template>
+
+					<template #item.time="{item, value}">
+						{{ moment__parseTime(value) }}
+					</template>
+
+					<!-- <template #item.action="{item, value}">
+					</template>
+
+					<template #item.agent="{item, value}">
+					</template> -->
 
 				</v-data-table>
 			</template>
@@ -17,34 +32,86 @@
 
 <script>
 import moment from "moment";
+
 export default {
 	data() {
 		return {
-			headers: [
-				{
-					text: "Date",
-					align: "start",
-					sortable: false,
-					value: "date"
-				},
-				{ text: "Time", value: "time" },
-				{ text: "Description", value: "description" },
-				{ text: "Action", value: "action" },
-				{ text: "Agent", value: "agent" }
-			],
-			employees: [
-				{
-					date: moment()
-						.endOf("year")
-						.format("LL"),
-					time: moment().format("LTS"),
-					description:
-						"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
-					action: "DELETE",
-					agent: "Dolores, Ipsum Lorem"
-				}
-			]
+			loading: true,
+			systemLogs: [],
+			options: {
+				headers: [
+					{
+						text: "Date",
+						align: "start",
+						sortable: false,
+						value: "date"
+					},
+					{ text: "Time", value: "time" },
+					{ text: "Description", value: "description" },
+					{ text: "Action", value: "action" },
+					{ text: "Agent", value: "agent" }
+				],
+				data: [
+					{
+						date: moment()
+							.endOf("year")
+							.format("LL"),
+						time: moment().format("LTS"),
+						description:
+							"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod",
+						action: "DELETE",
+						agent: "Dolores, Ipsum Lorem"
+					}
+				]
+			}
 		};
+	},
+	methods: {
+		loadTableData() {
+			let logs = this.systemLogs.map(item => {
+				return {
+					_id: item._id,
+					date: item.date,
+					time: item.date,
+					description: item.description,
+					action: item.action,
+					agent: item.isServer ? "System" : "{username}"
+				};
+			});
+
+			this.options.data = logs;
+		},
+		moment__parseDate(date) {
+			return moment(date).format("LL");
+		},
+		moment__parseTime(date) {
+			return moment(date).format("LTS");
+		}
+	},
+	mounted() {
+		this.loading = true;
+		this.$http
+			.get("/api/auditlogs", {
+				params: {
+					userId: this.$store.state.user.userId,
+					access_token: localStorage.getItem("access_token")
+				}
+			})
+			.then(response => {
+				console.log(response);
+				if (response.data && typeof response.data === "object") {
+					this.systemLogs = response.data;
+					this.loadTableData();
+				} else {
+					this.loadTableData();
+				}
+
+				this.loading = false;
+			})
+			.catch(error => {
+				console.error(error);
+				this.loading = false;
+			});
 	}
 };
 </script>
