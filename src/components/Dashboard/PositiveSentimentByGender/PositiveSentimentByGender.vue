516424ec02352f7d8d800bc3f140d9cbdb9df3c3
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-05-28 17:06:18 +0800

    initial positive sentiment chart fix

--- src/components/Dashboard/PositiveSentimentByGender/PositiveSentimentByGender.vue
+++ src/components/Dashboard/PositiveSentimentByGender/PositiveSentimentByGender.vue
@@ -11,8 +11,12 @@
 
 <script>
 import apexchart from "vue-apexcharts";
+import moment from "moment";
 
 export default {
+	components: {
+		apexchart
+	},
 	data() {
 		return {
 			series: [
@@ -67,7 +71,7 @@ export default {
 					width: 3
 				},
 				xaxis: {
-					type: "numeric",
+					type: "datetime",
 					labels: {
 						show: true,
 						style: {
@@ -108,10 +112,121 @@ export default {
 			}
 		};
 	},
-	components: {
-		apexchart
+	computed: {},
+	methods: {
+		initializeChart() {
+			let allLogs = this.$store.getters["employees/attendanceLogs"];
+			let filtered = [];
+
+			let startDate = moment()
+				.subtract(2, "weeks")
+				.startOf("day");
+			let endDate = moment().endOf("day");
+
+			let male = [];
+			let female = [];
+
+			if (allLogs.length > 0) {
+				filtered = allLogs.filter(
+					item =>
+						moment(item.dateCreated).isSameOrAfter(startDate) &&
+						moment(item.dateCreated).isSameOrBefore(endDate)
+				);
+			}
+
+			filtered.forEach(item => {
+				if (item.employeeRef.isMale) {
+					male.push(item);
+				} else {
+					female.push(item);
+				}
+			});
+
+			let male_positiveSentiments = [];
+			let female_positiveSentiments = [];
+
+			male_positiveSentiments = male.map(
+				({ emotionIn, emotionOut, dateCreated }) => {
+					let satisfaction = 0;
+
+					switch (emotionIn) {
+						case 3:
+							satisfaction += 1;
+							break;
+						case 4:
+							satisfaction += 2;
+							break;
+						case 5:
+							satisfaction += 3;
+							break;
+					}
+
+					switch (emotionOut) {
+						case 3:
+							satisfaction += 1;
+							break;
+						case 4:
+							satisfaction += 2;
+							break;
+						case 5:
+							satisfaction += 3;
+							break;
+					}
+
+					return {
+						x: dateCreated,
+						y: satisfaction
+					};
+				}
+			);
+
+			female_positiveSentiments = female.map(
+				({ emotionIn, emotionOut, dateCreated }) => {
+					let satisfaction = 0;
+
+					switch (emotionIn) {
+						case 3:
+							satisfaction += 1;
+							break;
+						case 4:
+							satisfaction += 2;
+							break;
+						case 5:
+							satisfaction += 3;
+							break;
+					}
+
+					switch (emotionOut) {
+						case 3:
+							satisfaction += 1;
+							break;
+						case 4:
+							satisfaction += 2;
+							break;
+						case 5:
+							satisfaction += 3;
+							break;
+					}
+
+					return {
+						x: dateCreated,
+						y: satisfaction
+					};
+				}
+			);
+
+			this.series[0].data = male_positiveSentiments.sort(
+				(a, b) => new Date(a.dateCreated) - new Date(b.dateCreated)
+			);
+			this.series[1].data = female_positiveSentiments.sort(
+				(a, b) => new Date(a.dateCreated) - new Date(b.dateCreated)
+			);
+		}
 	},
-	methods: {}
+	async mounted() {
+		await this.$store.dispatch("employees/FETCH_ATTENDANCELOGS");
+		this.initializeChart();
+	}
 };
 </script>
 
