c0bf2a9eb79a95a87b93f0cbabbd2ece4e66ed98
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-05-28 17:27:03 +0800

    remove comments

--- src/components/Dashboard/OverallChart/options.js
+++ src/components/Dashboard/OverallChart/options.js
@@ -7,64 +7,31 @@ let data = [
 	{ name: "Unsubmitted", data: [] }
 ];
 
-// let data = [
-// 	{
-// 		name: "Angry",
-// 		data: [47, 44, 26, 21, 99, 85, 72, 96, 53, 20, 90, 81]
-// 	},
-// 	{
-// 		name: "Unhappy",
-// 		data: [88, 67, 24, 11, 54, 89, 79, 71, 32, 73, 22, 30]
-// 	},
-// 	{
-// 		name: "Okay",
-// 		data: [56, 92, 92, 97, 39, 65, 95, 39, 28, 95, 93, 96]
-// 	},
-// 	{
-// 		name: "Happy",
-// 		data: [84, 59, 91, 18, 26, 18, 2, 52, 36, 63, 2, 12]
-// 	},
-// 	{
-// 		name: "Amazing",
-// 		data: [96, 44, 98, 38, 40, 84, 90, 83, 19, 8, 45, 31]
-// 	},
-// 	{
-// 		name: "Unsubmitted",
-// 		data: [129, 150, 169, 315, 242, 159, 162, 332, 99, 89, 11, 77]
-// 	}
-// ];
-
 export function updateData(logs) {
 	if (!logs) {
 		return data;
 	}
 
-	// get all the emotions per month
 	let emotions = {
 		'unsubmitted': [[], [], [], [], [], [], [], [], [], [], [], []],
-		'angry': [[], [], [], [], [], [], [], [], [], [], [], []],
-		'unhappy': [[], [], [], [], [], [], [], [], [], [], [], []],
-		'okay': [[], [], [], [], [], [], [], [], [], [], [], []],
-		'happy': [[], [], [], [], [], [], [], [], [], [], [], []],
-		'amazing': [[], [], [], [], [], [], [], [], [], [], [], []],
+		'angry'      : [[], [], [], [], [], [], [], [], [], [], [], []],
+		'unhappy'    : [[], [], [], [], [], [], [], [], [], [], [], []],
+		'okay'       : [[], [], [], [], [], [], [], [], [], [], [], []],
+		'happy'      : [[], [], [], [], [], [], [], [], [], [], [], []],
+		'amazing'    : [[], [], [], [], [], [], [], [], [], [], [], []],
 	}
 
 	logs.forEach(item => {
-		// let stringMonth = (new Date(item.dateCreated).toDateString().substring(4, 7).toLowerCase());
 		let numericMonth = (new Date(item.dateCreated).getUTCMonth());
 		let emotionIn = Object.keys(emotions)[item.emotionIn];
 		let emotionOut = Object.keys(emotions)[item.emotionOut];
-		// console.log(numericMonth, emotionIn, emotionOut);
 
 		emotions[emotionIn][numericMonth].push(item.emotionIn);
 		emotions[emotionOut][numericMonth].push(item.emotionOut);
 	});
 
-	// console.log(emotions);
-
 	data.forEach((item) => {
 		emotions[item.name.toLowerCase()].forEach((arrayItem, index) => {
-			// console.log(arrayItem.length);
 			item.data[index] = arrayItem.length;
 		});
 	});
