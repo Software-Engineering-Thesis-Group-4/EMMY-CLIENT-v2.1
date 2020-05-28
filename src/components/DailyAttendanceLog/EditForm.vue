e649a1a2af62bc2464424176eefa4f103a901cfc
Nathaniel Saludes <saludesnathaniel@gmail.com>
2020-05-28 17:00:33 +0800

    create edit employee log form

---
+++ src/components/DailyAttendanceLog/EditForm.vue
@@ -0,0 +1,21 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	props: ["activate", "employeelog_ref"],
+	data() {
+		return {}
+	},
+	computed: {
+
+	},
+	methods: {
+		
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
